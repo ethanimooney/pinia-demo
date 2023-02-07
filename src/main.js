51cc91a7259e6d4ba708e7a19f33c81b8ccbb7b7
ethanimooney <ethanimooney@gmail.com>
2023-02-07 16:37:05 -0600

    Update pinia config

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import Vue from "vue";
 import { createApp } from "vue";
-import { createPinia, PiniaVuePlugin } from "pinia";
 
 import App from "./App.vue";
 
+import { createPinia, PiniaVuePlugin } from "pinia";
+import VueCompositionAPI from "@vue/composition-api";
+
 Vue.use(PiniaVuePlugin);
 const pinia = createPinia();
+Vue.use(VueCompositionAPI);
 const app = createApp(App);
 
-app.use(pinia);
 app.mount("#app");
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
   pinia,
+  render: (h) => h(App),
 }).$mount("#app");
